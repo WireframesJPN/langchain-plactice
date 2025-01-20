e1ce8ebf2b7b1cedb5afdef30b853d6bee3189f0
Junzo Matsunoo <junzo.matsunoo@wireframes.co.jp>
2025-01-21 08:33:17 +0900

    fix some code

--- app/main.py
+++ app/main.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 from langchain.llms import OpenAI
 
 # .envファイルを読み込む
-load_dotenv()
+# load_dotenv()
 
 # 環境変数からAPIキーを取得
 api_key = os.getenv("OPENAI_API_KEY")
+
+print("API Keyを取得-------")
+print(api_key)
 
 # OpenAIクライアントの設定
 llm = OpenAI(temperature=0.7, openai_api_key=api_key)
